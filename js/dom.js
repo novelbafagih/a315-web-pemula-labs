2b8aee89427d0f416ec85dda3f6ce62234be955d
novel <novelbafagih@student.ub.ac.id>
2021-06-18 10:51:21 +0700

    testing title and date value

---
+++ js/dom.js
@@ -0,0 +1,6 @@
+function addTodo(){
+	const title = document.getElementById("title").value;
+	const date = document.getElementById("date").value;
+	console.log(title);
+	console.log(date);
+}
---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded",function() {
+	const submitForm = document.getElementById("form");
+
+	submitForm.addEventListener("submit",(event) => {
+		event.preventDefault();
+		addTodo();
+	});
+})
