fa86d64e9dae9db6627f95adad73f74add1d22e9
novel <novelbafagih@student.ub.ac.id>
2021-06-18 11:27:31 +0700

    adding todo list item

--- js/dom.js
+++ js/dom.js
@@ -3,4 +3,28 @@ function addTodo(){
 	const date = document.getElementById("date").value;
 	console.log(title);
 	console.log(date);
+	const todos = document.getElementById("todos");
+	const item = makeTodo(title,date);
+
+	todos.appendChild(item);
 }
+
+function makeTodo(title,date){
+	const todoTitle = document.createElement("h2");
+	todoTitle.innerText = title;
+
+	const todoDate = document.createElement("p");
+	todoDate.innerText = date;	
+
+	const textContainer = document.createElement("div");
+	textContainer.classList.add("inner");
+	textContainer.appendChild(todoTitle);
+	textContainer.appendChild(todoDate);
+
+	const container = document.createElement("div");
+	container.classList.add("item","shadow");
+	container.appendChild(textContainer);
+
+	return container;
+}
+
